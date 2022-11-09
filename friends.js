5c35fb0154d20c009651825214047bf22605e3f2
Noah Byers <noahxbyers@gmail.com>
2022-11-09 15:29:25 -0800

    Add Array of Places.

--- friends.js
+++ friends.js
@@ -2,3 +2,8 @@ var arrays = ["Allen", "Steve", "Greg", "Kyler", "Matt"]
   console.log(arrays[2])
   console.log(arrays[4])
   console.log(arrays[0])
+
+var places = ["Brazil","Australia","Japan","Big Sky","Moon"]
+  places.pop()
+    console.log(places)
+  
